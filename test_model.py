fb36166782879ff7b9a1f56a23aeb45eeb5d5c5a
Sankulp <sankulp99@gmail.com>
2025-09-18 19:16:18 +0530

    Delete test_model.py

--- test_model.py
+++
@@ -1,6 +0,0 @@
-from app import predict_fake_news
-
-def test_prediction():
-    text = "Breaking news: the world is flat!"
-    result = predict_fake_news(text)
-    assert result in [0, 1]  # 0 = real, 1 = fake
